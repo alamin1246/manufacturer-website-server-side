cfef6785533580f92f151441fac382893c959e87
alamin1246 <hossainalamin1246@gmail.com>
2022-05-26 00:38:32 +0600

    mongodb database setup in server

--- index.js
+++ index.js
@@ -13,6 +13,30 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x9k6n.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+
+const run = async () => {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDb Successfully");
+    const database = client.database("computerPartsManufacturer");
+    const productsCollection = database.collection("products");
+    const ordersCollection = database.collection("orders");
+    const usersCollection = database.collection("users");
+    const reviewsCollection = database.collection("reviews");
+    const blogsCollection = database.collection("blogs");
+    const adminsCollection = database.collection("admins");
+
+
+  } finally {
+    // client.close();
+  }
+};
+
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Listening Computer Parts Manufacturer on port ${port}`);
